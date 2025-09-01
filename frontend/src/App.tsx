900f21acd6a356d6b0e7688be0c6f544d9f6a17d
nithish9020 <nithishkumar9020@gmail.com>
2025-09-01 19:06:40 +0530

    auto redirect based on localstorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-
+import Dashboard from "./pages/Dashboard";
+import { AuthProvider } from "@/context/AuthContext";
+import AuthRedirect from "@/components/AuthRedirect";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<AuthRedirect />} />
+          <Route
+            path='/dashboard'
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
---
+++ frontend/src/components/AuthRedirect.tsx
@@ -0,0 +1,9 @@
+import { useAuth } from "@/context/AuthContext";
+import LoginPage from "@/pages/LoginPage";
+import { Navigate } from "react-router-dom";
+
+export default function AuthRedirect() {
+  const { token, loading } = useAuth();
+  if (loading) return <div>Loading...</div>;
+  return token ? <Navigate to="/dashboard" replace /> : <LoginPage />;
+}
